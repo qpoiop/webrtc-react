ac22ab5986b1385febf710dcb85de987d4ef5be9
qpoiop1 <qpoiop1@gmail.com>
2021-06-01 13:47:43 +0900

    chore: Create react project with next.js

---
+++ components/layout/header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Header = class extends React.Component {
+  constructor() {}
+  componentDidMount() {
+
+  }
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+
+export default Header
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Index() {
+  return <div> Index page </div>
+}
+
+export default Index
